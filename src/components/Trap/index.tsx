73178fcd09b215042284510be46a5f4eeb83bb8d
Vinícius Melo <vinicius.apol.melo@gmail.com>
2022-06-30 21:24:21 -0300

    :lipstick: feat: Animações das Traps

--- src/components/Trap/index.tsx
+++ src/components/Trap/index.tsx
@@ -7,17 +7,17 @@ function Trap({ initialPosition }) {
   return (
     <div>
       <div
-        className="hero"
+        className="trap"
         style={{
           position: 'absolute',
           top: TILE_SIZE * initialPosition.y,
           left: TILE_SIZE * initialPosition.x,
           width: TILE_SIZE,
-          height: TILE_SIZE * 2,
-          backgroundImage: 'url(./assets/images/trap.png)',
+          height: TILE_SIZE,
+          // backgroundImage: 'url(./assets/images/trap.png)',
           backgroundRepeat: 'no-repeat',
           backgroundPosition: '0 0',
-          animation: 'trap 2s steps(4) infinite easy-in',
+          animation: 'trap 1s steps(4) infinite',
         }}
       />
       <img src="../../" alt="" />
